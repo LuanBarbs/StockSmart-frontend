d4d9fa32313177ae0022be142c56eda4b7c67e4c
jaonorton <jaonorton@gmail.com>
2025-01-26 18:59:28 -0300

    Funcionando o visual

--- components/employee/MoveItems.js
+++ components/employee/MoveItems.js
@@ -1,28 +1,23 @@
 import React, { useState, useEffect } from "react";
-import { FaEye, FaBars, FaPlusSquare, FaTrash, FaEdit, FaWarehouse } from "react-icons/fa";
+import { FaEye, FaBars, FaPlusSquare, FaTrash, FaEdit, FaBox, FaWarehouse } from "react-icons/fa";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import styles from "../../styles/InitManager.module.css";
 import Item from "../../models/Item";
 import Warehouse from "../../models/Warehouse";
 
-/*
-Ideia: 
-1) selecionar uma warehouse
-2) selecionar um item, depois quantidade
-3) selecionar destino
-4) realizar verificação de é valido
-5) se não, perguntar se quer alterar quantidade ou selecionar outra warehouse
-6) se sim, trocar os itens
-*/
 
 export default function MoveItems() {
 
     const [showItems, setShowItems] = useState(false);
     const [showModal, setShowModal] = useState(false);
+    const [selectedItem, setSelectedItem] = useState(null);
+    const [selectItemModalOpen, setShowselectItemModalOpen] = useState(false);
+    const [editItem, setEditItem] = useState(null);
+    const [selectDestinyModalOpen, setSelectDestinyModalOpen] = useState(false);
+    
     
     const [warehouses, setWarehouses] = useState([]);
     const [showWarehousesOrigin, setShowWarehousesOrigin] = useState(true);
-    const [showWarehousesDestiny, setShowWarehousesDestiny] = useState(false);
     const [selectedWarehouse, setSelectedWarehouse] = useState(null);
    
     const [items, setItems] = useState([]);
@@ -30,7 +25,6 @@ export default function MoveItems() {
     const handleToggleItems = () => setShowItems(!showItems);
 
     const handleToggleWarehousesOrigin = () => setShowWarehousesOrigin(!showWarehousesOrigin);
-    const handleToggleWarehousesDestiny = () => setShowWarehousesDestiny(!showWarehousesDestiny);
 
     const handleOpenItemModal = (item) => {
         setSelectedItem(item);
@@ -40,15 +34,50 @@ export default function MoveItems() {
         setShowModal(false);
     };
 
-    const handleEditItem = (item) => {
-        setEditItem(item);
-        setEditModalOpen(true);
+    const handleSelectWarehouse = (warehouse) => {
+        const newSelectedWarehouse = new Warehouse(
+            warehouse.id,
+            warehouse.name,
+            warehouse.location,
+            warehouse.capacity,
+            warehouse.currentCapacity,
+            warehouse.zones,
+            warehouse.status,
+            warehouse.createdAt,
+        );
+        setShowWarehousesOrigin(false);
+        const itemTeste1 = new Item(1, "Teste'", 10, 1, "Eletronico", 100, 12/1/2122, null, 1 );
+        const itemTeste2 = new Item(2, "Teste2'", 20, 1, "Teste", 200, 12/1/2122, null, 1 );
+        newSelectedWarehouse.addItem(itemTeste1);
+        newSelectedWarehouse.addItem(itemTeste2);
+        setItems(newSelectedWarehouse.items);
+        setShowItems(true)
     };
 
-    const handleSelectItem = (warehouse) => {
-        setShowWarehousesOrigin(false);
-        setItems(warehouse.items);
-        setShowItems(true)
+    const handleSelectItem = (item) => {        
+        setEditItem(item);
+        setSelectDestinyModalOpen(true);
+    };
+
+    const handleUpdateItem = async () => {
+        if (editItem.quantity <= 0 || !editItem.name || !editItem.warehouseId || !editItem.category || !editItem.volume || !editItem.expirationDate) {
+            alert("Erro: Todos os campos devem ser preenchidos corretamente.");
+            return;
+        }
+
+        const updatedItems = items.map(item =>
+            item.id === editItem.id ? editItem : item
+        );
+        setItems(updatedItems);
+        await AsyncStorage.setItem('items', JSON.stringify(updatedItems));
+        setSelectDestinyModalOpen(false);
+        handleCloseModal();
+        alert("Alteração realizada com sucesso!");
+    };
+
+    const backSelectWarehouse = () => {
+        setShowItems(false)
+        setShowWarehousesOrigin(true)
     };
 
     const handleOpenWarehouseModal = (warehouse) => {
@@ -87,6 +116,9 @@ export default function MoveItems() {
             <section className={styles.content}>
                 {showWarehousesOrigin && (
                     <section className={`${showWarehousesOrigin ? styles.formContainer : styles.fullFormContainer}`}>
+                        <button className={styles.backButton} onClick={() => backSelectWarehouse()}>
+                            ← Voltar
+                        </button>
                         <h1>Armazém de origem</h1>
                         <hr color="#f1f1b1" size="5"/>
                         <ul>
@@ -102,7 +134,7 @@ export default function MoveItems() {
                                         </button>
                                         <button
                                             className={styles.selectButton}
-                                            onClick={() => handleSelectItem(warehouse)}    >
+                                            onClick={() => handleSelectWarehouse(warehouse)}    >
                                             Selecionar
                                         </button>
                                     </li>
@@ -115,48 +147,26 @@ export default function MoveItems() {
                     </section>
                 )}
 
-                {showWarehousesDestiny && (
-                    <section className={`${showWarehousesDestiny ? styles.formContainer : styles.fullFormContainer}`}>
-                        <h1>Armazém de destino</h1>
-                        <hr color="#f1f1b1" size="5"/>
-                        <ul>
-                            {warehouses.length != 0 && (
-                                warehouses.map((warehouse, index) => (
-                                    <li key={index} className={styles.warehouseBox}>
-                                        <span>{warehouse.id} - Nome: {warehouse.name}</span>
-                                        <button
-                                            className={styles.eyeButton}
-                                            onClick={() => handleOpenWarehouseModal(warehouse)}
-                                        >
-                                            <FaEye />
-                                        </button>
-                                    </li>
-                                ))
-                            )}
-                            {warehouses.length == 0 && (
-                                <p>Não há armazéns cadastrados!</p>
-                            )}
-                        </ul>
-                    </section>
-                )}
-
                 {showModal && selectedWarehouse && (
-                <section className={styles.modal}>
-                    <div className={styles.modalContent}>
-                        <button className={styles.closeButton} onClick={handleCloseModal}>X</button>
-                        <FaWarehouse size={100}/>
-                        <h2>{selectedWarehouse?.name}</h2>
-                        <p>Endereço: {selectedWarehouse?.location}</p>
-                        <p>Capacidade Total (em m^3): {selectedWarehouse?.capacity}</p>
-                        <p>Capacidade Disponível (em m^3): {selectedWarehouse?.getAvailableCapacity()}</p>
-                        <p>Quantidade de zonas: {selectedWarehouse?.zones.length}</p>
-                        <p>Status: {selectedWarehouse?.status === "active" ? "Ativo" : "Inativo"}</p>                        
-                    </div>
-                </section>
-            )}     
+                    <section className={styles.modal}>
+                        <div className={styles.modalContent}>
+                            <button className={styles.closeButton} onClick={handleCloseModal}>X</button>
+                            <FaWarehouse size={100}/>
+                            <h2>{selectedWarehouse?.name}</h2>
+                            <p>Endereço: {selectedWarehouse?.location}</p>
+                            <p>Capacidade Total (em m^3): {selectedWarehouse?.capacity}</p>
+                            <p>Capacidade Disponível (em m^3): {selectedWarehouse?.getAvailableCapacity()}</p>
+                            <p>Quantidade de zonas: {selectedWarehouse?.zones.length}</p>
+                            <p>Status: {selectedWarehouse?.status === "active" ? "Ativo" : "Inativo"}</p>                        
+                        </div>
+                    </section>
+                )}     
                 
-                {showItems && (         // não tá salvando os itens cadastrados
+                {showItems && (        
                     <section className={`${showItems ? styles.warehousesContainer : styles.fullWarehousesContainer}`}>
+                        <button className={styles.backButton} onClick={() => backSelectWarehouse()}>
+                            ← Voltar
+                        </button>
                         <h1>Itens Cadastrados</h1>
                         <hr color="#f1f1b1" size="5"/>
                         <ul>
@@ -169,6 +179,11 @@ export default function MoveItems() {
                                             onClick={() => handleOpenItemModal(item)}
                                         >
                                             <FaEye />
+                                        </button>
+                                        <button
+                                            className={styles.selectButton}
+                                            onClick={() => handleSelectItem(item)}    >
+                                            Selecionar
                                         </button>
                                     </li>
                                 ))
@@ -194,9 +209,43 @@ export default function MoveItems() {
                         <p>Volume (unitário): {selectedItem?.volume}</p>
                         <p>Data de validade: {selectedItem?.expirationDate}</p>
                         <p>Status: {selectedItem?.status === "active" ? "Ativo" : "Inativo"}</p>
-                        <div className={styles.modalActions}>
-                            <button onClick={() => handleEditItem(selectedItem)}><FaEdit />Alterar</button>
-                        </div>
+                        
+                    </div>
+                </section>
+            )}
+
+            {selectDestinyModalOpen && (
+                <section className={styles.modal}>
+                    <div className={styles.modalContent}>
+                        <h2>Movimentar item</h2>
+                        <form
+                            onSubmit={(e) => {
+                                e.preventDefault();
+                                handleUpdateItem();
+                            }}
+                        >
+                            <label>Quantidade:</label>
+                            <input 
+                                type="number"
+                                value={editItem.quantity}
+                                onChange={(e) => setEditItem({...editItem, quantity: e.target.value})}
+                            />
+                            <label>Id do armazém de destino:</label>
+                            <select
+                                value={editItem.warehouseId}
+                                onChange={(e) => setEditItem({...editItem, warehouseId: e.target.value})} 
+                            >
+                                <option>Selecione um armazém</option>
+                                {warehouses.map((warehouse, index) => (
+                                    <option key={index} value={warehouse.id}>{warehouse.id} - {warehouse.name}</option>
+                                ))}
+                            </select>                            
+                                             
+                            <div className={styles.modalActions}>
+                                <button type="submit">Salvar</button>
+                                <button onClick={() => setSelectDestinyModalOpen(false)}>Cancelar</button>
+                            </div>
+                        </form>
                     </div>
                 </section>
             )}
